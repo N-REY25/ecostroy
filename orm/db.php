68ffc1191f7c2c71ca1d1c19731f00174d2eb3fc
Andrey Gerasimov <andrey.gerasimov2506@gmail.com>
2023-03-19 23:19:10 +0300

    Now committed

---
+++ orm/db.php
@@ -0,0 +1,4 @@
+<?php
+    require 'rb.php';
+    R::setup('mysql:host=192.168.0.13;dbname=ecostroy', 'root', '');
+    session_start();
---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(event) {
+    // console.log('Установка');
+});
+
+self.addEventListener('activate', (event) => {
+    // console.log('Активировация');
+});
+
+self.addEventListener('fetch', function(event) {
+    // console.log('Обработка');
+});
+
